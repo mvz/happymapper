38ae2653eb5f7630a2e9400e0c04641b346bd85f
Alex <alex@klepa.co>
2013-04-05 17:59:10 -0300

    Add nil check to handle_attributes_option

--- lib/happymapper/element.rb
+++ lib/happymapper/element.rb
@@ -34,6 +34,7 @@ module HappyMapper
     def handle_attributes_option(result, value, xpath_options)
       if options[:attributes].is_a?(Hash)
         result = result.first unless result.respond_to?(:attribute_nodes)
+        return unless result
 
         result.attribute_nodes.each do |xml_attribute|
           if attribute_options = options[:attributes][xml_attribute.name.to_sym]

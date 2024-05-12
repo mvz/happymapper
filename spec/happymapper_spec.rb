b8df33ac3ce2dda3844c3d636a987eed4bde7f36
Matijs van Zuijlen <matijs@matijs.net>
2024-05-12 13:10:32 +0200

    Extract regression spec for google analytics xml parsing

--- spec/happymapper_spec.rb
+++ spec/happymapper_spec.rb
@@ -3,72 +3,6 @@
 require "spec_helper"
 require "uri"
 
-module Analytics
-  class Property
-    include HappyMapper
-
-    tag "property"
-    namespace "dxp"
-    attribute :name, String
-    attribute :value, String
-  end
-
-  class Goal
-    include HappyMapper
-
-    # Google Analytics does a dirtry trick where a user with no goals
-    # returns a profile without any goals data or the declared namespace
-    # which means Nokogiri does not pick up the namespace automatically.
-    # To fix this, we manually register the namespace to avoid bad XPath
-    # expression. Dirty, but works.
-
-    register_namespace "ga", "http://schemas.google.com/ga/2009"
-    namespace "ga"
-
-    tag "goal"
-    attribute :active, Boolean
-    attribute :name, String
-    attribute :number, Integer
-    attribute :value, Float
-
-    def clean_name
-      name.gsub("ga:", "")
-    end
-  end
-
-  class Profile
-    include HappyMapper
-
-    tag "entry"
-    element :title, String
-    element :tableId, String, namespace: "dxp"
-
-    has_many :properties, Property
-    has_many :goals, Goal
-  end
-
-  class Entry
-    include HappyMapper
-
-    tag "entry"
-    element :id, String
-    element :updated, DateTime
-    element :title, String
-    element :table_id, String, namespace: "dxp", tag: "tableId"
-    has_many :properties, Property
-  end
-
-  class Feed
-    include HappyMapper
-
-    tag "feed"
-    element :id, String
-    element :updated, DateTime
-    element :title, String
-    has_many :entries, Entry
-  end
-end
-
 module Atom
   class Feed
     include HappyMapper
@@ -946,35 +880,6 @@ describe HappyMapper do
       expect(last_event.address.state).to eq("FL")
       expect(last_event.address.zip).to eq("327506398")
       expect(track.tran_detail.cust_tran_id).to eq("20090102-111321")
-    end
-  end
-
-  it "is able to parse google analytics api xml" do
-    data = Analytics::Feed.parse(fixture_file("analytics.xml"))
-
-    aggregate_failures do
-      expect(data.id).to eq("http://www.google.com/analytics/feeds/accounts/nunemaker@gmail.com")
-      expect(data.entries.size).to eq(4)
-
-      entry = data.entries[0]
-      expect(entry.title).to eq("addictedtonew.com")
-      expect(entry.properties.size).to eq(4)
-
-      property = entry.properties[0]
-      expect(property.name).to eq("ga:accountId")
-      expect(property.value).to eq("85301")
-    end
-  end
-
-  it "is able to parse google analytics profile xml with manually declared namespace" do
-    data = Analytics::Profile.parse(fixture_file("analytics_profile.xml"))
-
-    aggregate_failures do
-      expect(data.entries.size).to eq(6)
-      entry = data.entries[0]
-      expect(entry.title).to eq("www.homedepot.com")
-      expect(entry.properties.size).to eq(6)
-      expect(entry.goals.size).to eq(0)
     end
   end
 

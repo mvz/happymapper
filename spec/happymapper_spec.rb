72834e55c0ba9db5d5c3460c23159ff7570efcad
Matijs van Zuijlen <matijs@matijs.net>
2024-05-12 13:35:44 +0200

    Extract regression spec for github xml parsing

--- spec/happymapper_spec.rb
+++ spec/happymapper_spec.rb
@@ -241,19 +241,6 @@ module PITA
   end
 end
 
-module GitHub
-  class Commit
-    include HappyMapper
-
-    tag "commit"
-    element :url, String
-    element :tree, String
-    element :message, String
-    element :id, String
-    element :"committed-date", Date
-  end
-end
-
 module QuarterTest
   class Quarter
     include HappyMapper
@@ -748,18 +735,6 @@ describe HappyMapper do
     aggregate_failures do
       expect(game.q1.start).to eq("4:40:15 PM")
       expect(game.q2.start).to eq("5:18:53 PM")
-    end
-  end
-
-  it "parses xml that has elements with dashes" do
-    commit = GitHub::Commit.parse(fixture_file("commit.xml"))
-
-    aggregate_failures do
-      expect(commit.message).to eq("move commands.rb and helpers.rb into commands/ dir")
-      expect(commit.url).to eq("http://github.com/defunkt/github-gem/commit/c26d4ce9807ecf57d3f9eefe19ae64e75bcaaa8b")
-      expect(commit.id).to eq("c26d4ce9807ecf57d3f9eefe19ae64e75bcaaa8b")
-      expect(commit.committed_date).to eq(Date.parse("2008-03-02T16:45:41-08:00"))
-      expect(commit.tree).to eq("28a1a1ca3e663d35ba8bf07d3f1781af71359b76")
     end
   end
 

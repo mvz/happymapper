f2732728363035222a178a42a4aed512cc0a9e08
Matijs van Zuijlen <matijs@matijs.net>
2024-05-12 11:39:02 +0200

    Extract feature spec showing handling of optional attributes

---
+++ spec/features/optional_attributes_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.describe "Parsing optional attributes" do
+  before do
+    klass = Class.new do
+      include HappyMapper
+      tag "address"
+
+      attribute :street, String
+    end
+    stub_const "OptionalAttribute", klass
+  end
+
+  let(:parsed_result) { OptionalAttribute.parse(fixture_file("optional_attributes.xml")) }
+
+  it "parses an empty String as empty" do
+    expect(parsed_result[0].street).to eq("")
+  end
+
+  it "parses a String with value" do
+    expect(parsed_result[1].street).to eq("Milchstrasse")
+  end
+
+  it "parses an element with no value for the attribute" do
+    expect(parsed_result[2].street).to be_nil
+  end
+end
--- spec/happymapper_spec.rb
+++ spec/happymapper_spec.rb
@@ -527,13 +527,6 @@ class Video
   element :publish_options, PublishOptions, tag: "publishOptions", namespace: "video"
 end
 
-class OptionalAttribute
-  include HappyMapper
-  tag "address"
-
-  attribute :street, String
-end
-
 class DefaultNamespaceCombi
   include HappyMapper
 
@@ -1028,22 +1021,6 @@ describe HappyMapper do
   it "parses lastfm namespaces" do
     l = Location.parse(fixture_file("lastfm.xml"))
     expect(l.first.latitude).to eq("51.53469")
-  end
-
-  describe "Parse optional attributes" do
-    let(:parsed_result) { OptionalAttribute.parse(fixture_file("optional_attributes.xml")) }
-
-    it "parses an empty String as empty" do
-      expect(parsed_result[0].street).to eq("")
-    end
-
-    it "parses a String with value" do
-      expect(parsed_result[1].street).to eq("Milchstrasse")
-    end
-
-    it "parses an element with no value for the attribute" do
-      expect(parsed_result[2].street).to be_nil
-    end
   end
 
   describe "Default namespace combi" do

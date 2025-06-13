1ba02657415cd1fdefb1decd941e283497097105
Matijs van Zuijlen <matijs@matijs.net>
2025-06-13 13:39:18 +0200

    Autocorrect Style/EmptyStringInsideInterpolation

--- lib/happymapper.rb
+++ lib/happymapper.rb
@@ -207,7 +207,7 @@ module HappyMapper
       next if value.nil? && !attribute.options[:state_when_nil]
 
       attribute_namespace = attribute.options[:namespace]
-      ["#{attribute_namespace ? "#{attribute_namespace}:" : ""}#{attribute.tag}", value]
+      ["#{"#{attribute_namespace}:" if attribute_namespace}#{attribute.tag}", value]
     end
 
     attributes.to_h

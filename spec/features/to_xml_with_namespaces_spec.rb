mvz/happymapper

commit c0aef79a24b05aec729f2a20ba667fe5ff6cf0c9

Render namespace definitions only on the root element in JRuby
This already worked in CRuby.

lib/happymapper.rb
@@ -292,7 +292,10 @@ module HappyMapper
         # we should append the value for the tag
         #
         if item_namespace
-          xml[item_namespace].send(:"#{tag}_", item.to_s)
+          xml.send(:"#{tag}_", item.to_s) do |child_xml|
+            child_xml.parent.namespace =
+              xml.doc.root.namespace_definitions.find { |x| x.prefix == item_namespace }
+          end
         else
           xml.send(:"#{tag}_", item.to_s)
         end

spec/features/to_xml_with_namespaces_spec.rb
@@ -268,4 +268,35 @@ RSpec.describe "Saving #to_xml with xml namespaces" do
       expect(machine.to_xml).to eq(expected_xml)
     end
   end
+
+  context "with namespace set on base type child elements" do
+    let(:klass) do
+      Class.new do
+        include HappyMapper
+
+        register_namespace "prefix", "http://www.unicornland.com/prefix"
+        tag :foo
+        has_one :bar, String, namespace: "prefix"
+      end
+    end
+
+    it "renders xml that can be parsed by the same class" do
+      obj = klass.new
+      obj.bar = "foobar"
+      copy = klass.parse(obj.to_xml)
+      expect(copy.bar).to eq obj.bar
+    end
+
+    it "renders the namespace definition only on the root element" do
+      obj = klass.new
+      obj.bar = "foobar"
+
+      expect(obj.to_xml).to eq <<~XML
+        <?xml version="1.0"?>
+        <foo xmlns:prefix="http://www.unicornland.com/prefix">
+          <prefix:bar>foobar</prefix:bar>
+        </foo>
+      XML
+    end
+  end
 end

commit 26f496e9bf5af33c82b622e997f04b9b2eb27e8d

Allow setting child element namespace using a symbol
This used to already work when setting the namespace on a basic type
element, but this was untested and broke with the change in commit
c0aef79a24b05aec729f2a20ba667fe5ff6cf0c9.

lib/happymapper.rb
@@ -207,7 +207,7 @@ module HappyMapper
       #
       next if value.nil? && !attribute.options[:state_when_nil]
 
-      attribute_namespace = attribute.options[:namespace]
+      attribute_namespace = attribute.namespace
       attribute_name = if attribute_namespace
                          "#{attribute_namespace}:#{attribute.tag}"
                        else
@@ -277,13 +277,13 @@ module HappyMapper
         # to the builder structure
         #
         item.to_xml(xml, self.class.namespace || default_namespace,
-                    element.options[:namespace],
+                    element.namespace,
                     element.options[:tag] || nil)
 
       elsif !item.nil? || element.options[:state_when_nil]
 
         item_namespace =
-          element.options[:namespace] ||
+          element.namespace ||
           self.class.namespace ||
           default_namespace
 

lib/happymapper/element.rb
@@ -3,12 +3,7 @@
 module HappyMapper
   class Element < Item
     def find(node, namespace, xpath_options)
-      if self.namespace
-        # from the class definition
-        namespace = self.namespace
-      elsif options[:namespace]
-        namespace = options[:namespace]
-      end
+      namespace = self.namespace if self.namespace
 
       if options[:single]
         result = if options[:xpath]

lib/happymapper/item.rb
@@ -18,6 +18,7 @@ module HappyMapper
       self.type = type
       # self.tag = options.delete(:tag) || name.to_s
       self.tag = options[:tag] || name.to_s
+      self.namespace = options[:namespace]&.to_s
       self.options = { single: true }.merge(options.merge(name: self.name))
 
       @xml_type = self.class.to_s.split("::").last.downcase
@@ -33,7 +34,7 @@ module HappyMapper
     # @param [Hash] xpath_options additional xpath options
     #
     def from_xml_node(node, namespace, xpath_options)
-      namespace = options[:namespace] if options.key?(:namespace)
+      namespace = self.namespace if options.key?(:namespace)
 
       if custom_parser_defined?
         find(node, namespace, xpath_options) { |n| process_node_with_custom_parser(n) }

spec/features/to_xml_with_namespaces_spec.rb
@@ -269,6 +269,37 @@ RSpec.describe "Saving #to_xml with xml namespaces" do
     end
   end
 
+  context "with namespace set on child elements using a symbol" do
+    before do
+      stub_const "Beverage", Class.new {
+        include HappyMapper
+
+        attribute :name, String
+      }
+
+      stub_const "CoffeeMachine", Class.new {
+        include HappyMapper
+
+        register_namespace "beverage", "http://beverages.org/Beverage/0.1"
+
+        element :beverage, "beverage", namespace: :beverage
+      }
+    end
+
+    it "uses the element declaration namespace on the element" do
+      machine = CoffeeMachine.new
+      machine.beverage = Beverage.new.tap { |obj| obj.name = "coffee" }
+      expected_xml = <<~XML
+        <?xml version="1.0"?>
+        <coffeemachine xmlns:beverage="http://beverages.org/Beverage/0.1">
+          <beverage:beverage name="coffee"/>
+        </coffeemachine>
+      XML
+
+      expect(machine.to_xml).to eq(expected_xml)
+    end
+  end
+
   context "with namespace set on base type child elements" do
     let(:klass) do
       Class.new do
@@ -300,7 +331,38 @@ RSpec.describe "Saving #to_xml with xml namespaces" do
     end
   end
 
-  context "with namespace set on the root object using a symbol" do
+  context "with namespace set on base type child elements using a symbol" do
+    let(:klass) do
+      Class.new do
+        include HappyMapper
+
+        register_namespace "prefix", "http://www.unicornland.com/prefix"
+        tag :foo
+        has_one :bar, String, namespace: :prefix
+      end
+    end
+
+    it "renders xml that can be parsed by the same class" do
+      obj = klass.new
+      obj.bar = "foobar"
+      copy = klass.parse(obj.to_xml)
+      expect(copy.bar).to eq obj.bar
+    end
+
+    it "renders the namespace definition only on the root element" do
+      obj = klass.new
+      obj.bar = "foobar"
+
+      expect(obj.to_xml).to eq <<~XML
+        <?xml version="1.0"?>
+        <foo xmlns:prefix="http://www.unicornland.com/prefix">
+          <prefix:bar>foobar</prefix:bar>
+        </foo>
+      XML
+    end
+  end
+
+  context "with namespace set on the root object using a string" do
     let(:klass) do
       Class.new do
         include HappyMapper